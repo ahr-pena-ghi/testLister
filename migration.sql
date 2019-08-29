8d9f99cb7c7da830876b2f4377d592d11f889d81
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-08-29 11:49:44 -0500

    added migration file

--- migration.sql
+++ migration.sql
@@ -2,12 +2,17 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS cats_ads;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -16,7 +21,25 @@ CREATE TABLE ads (
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
+    price DECIMAL(6,2) NOT NULL,
+    listed_on DATETIME DEFAULT NOW(),
+    picture TEXT,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE cats_ads (
+    cat_id INT UNSIGNED NOT NULL,
+    ad_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (cat_id) REFERENCES categories(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (ad_id) REFERENCES ads(id)
         ON DELETE CASCADE
 );
